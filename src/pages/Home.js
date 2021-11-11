4d6abd2ac43af341fc9b7b943dba2fa13034dff0
anguyn <90667415+anguyn@users.noreply.github.com>
2021-11-11 10:19:58 +0700

    src 5

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import MainLayout from "../layouts/MainLayout";
+
+function Home() {
+
+    return (
+        <>
+        <MainLayout>
+            <h1>Home</h1>
+        </MainLayout>
+        </>
+        // trong pages sẽ chứa layout
+    );
+}
+
+export default Home;
