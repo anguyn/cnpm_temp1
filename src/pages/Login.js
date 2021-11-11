a93d91e3fa702dbf6a4b6f979302685a4946edf1
anguyn <90667415+anguyn@users.noreply.github.com>
2021-11-11 10:20:50 +0700

    src 6

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+function Login () {
+    return (
+        <>
+            <h1>Login</h1>
+            {/* trong page login gọi layout login nhưng page login khác với các trang trong web */}
+            {/* nên sẽ ko có layout */}
+            <h2>Hello</h2>
+        </>
+    )
+}
+
+export default Login;
